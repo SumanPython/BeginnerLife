0c6c7a50f8869ce49a637169df33727727ba1cf8
SumanPython <34559320+SumanPython@users.noreply.github.com>
2020-04-28 20:45:53 +0530

    "adding minor changes"

--- Calculator/Addition.py
+++ Calculator/Addition.py
@@ -1,3 +1,2 @@
-#addition
-def Addition(num1, num2):
+def addition(num1, num2):
     print("The Addition of two numbers is ",(num1 + num2))
--- Calculator/Modulo.py
+++ Calculator/Modulo.py
@@ -1,3 +1,3 @@
-#module
+#modulo
 def modulo(num1, num2):
     print("The modulo of two numbers is ",(num1 % num2))
